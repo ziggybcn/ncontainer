e9bf0e566638c68cfbef430df39718fa4314b03d
Manel Ibáñez <mibanez@biosystems.es>
2017-07-14 13:04:11 +0200

    First commit (draft and idea)

---
+++ Pico/Properties/AssemblyInfo.cs
@@ -0,0 +1,20 @@
+using System.Reflection;
+using System.Runtime.CompilerServices;
+using System.Runtime.InteropServices;
+
+[assembly: AssemblyTitle("Pico")]
+[assembly: AssemblyDescription("")]
+[assembly: AssemblyConfiguration("")]
+[assembly: AssemblyCompany("Microsoft")]
+[assembly: AssemblyProduct("Pico")]
+[assembly: AssemblyCopyright("Copyright © Microsoft 2017")]
+[assembly: AssemblyTrademark("")]
+[assembly: AssemblyCulture("")]
+
+[assembly: ComVisible(false)]
+
+[assembly: Guid("613d2e4a-7f29-46b3-a568-cbc182d900d6")]
+
+// [assembly: AssemblyVersion("1.0.*")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ Pico/UnitTest1.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace Pico
+{
+    [TestClass]
+    public class UnitTest1
+    {
+        [TestMethod]
+        public void TestMethod1()
+        {
+        }
+    }
+
+    public class Container
+    {
+        public Container Register(Type myType)
+        {
+            foreach (var cosa in myType.GetInterfaces())
+                Register(myType, cosa);
+            return this;
+        }
+
+        public Container Register(Type myType, Type myInterface)
+        {
+            if (!myInterface.IsInterface) throw new ExpectingInterfaceException("Expecting an interface type");
+            portsAndAddapters.ContainsKey(myInterface)
+            return this;
+        }
+
+        //private readonly PortsAndAddapters portsAndAddapters = new PortsAndAddapters();
+    }
+
+    internal class Port
+    {
+        private readonly List<Addapter> addapters = new List<Addapter>();
+        public readonly Type Contract;
+        public Port(Type contract)
+        {
+            Contract = contract;
+        }
+    }
+
+    internal class Addapter
+    {
+        public Addapter(Type implementation)
+        {
+            Implementation = implementation;
+        }
+
+        public readonly Type Implementation;
+
+    }
+
+
+    public class ExpectingInterfaceException : Exception
+    {
+        public ExpectingInterfaceException(string details):base(details) { }
+
+    }
+
+
+    internal class TestClassA: TestInterfaceA
+    {
+        TestClassA()
+        {
+            
+        }
+    }
+
+    internal class DependantClass : DependantInterface
+    {
+        DependantClass(TestInterfaceA myTestClass)
+        {
+            
+        }
+    }
+
+    public interface TestInterfaceA { }
+
+    public interface DependantInterface { }
+
+}
+
+
