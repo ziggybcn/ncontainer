ca5d2150069918ddfa9341e632874933121e3d5b
Manel Ibáñez <m.ibanez@blide.org>
2017-07-22 21:27:55 +0200

    Migrated from MSTest to NUnit with fluent assertions

---
+++ NContainerTests/NContainerTests/PerformanceTests.cs
@@ -0,0 +1,41 @@
+﻿using NContainer;
+using NUnit.Framework;
+
+namespace NContainerTests {
+    [TestFixture]
+    public class PerformanceTests {
+        private const int Iterations = 4000000;
+
+        [Test]
+        public void MeasureReflectionInstanceResolver() {
+            var c = new Container().Register<TestClassA>().Register<DependantClass>();
+            for (var i = 0; i < Iterations; i++)
+                c.GetInstance<DependantInterface>();
+        }
+
+        [Test]
+        public void MeasureSingletoneDependencyInstanceResolver() {
+            var c = new Container().Register<TestInterfaceA>(new TestClassA()).Register<DependantClass>();
+            for (var i = 0; i < Iterations; i++)
+                c.GetInstance<DependantInterface>();
+        }
+
+        [Test]
+        public void MeasureSingletoneInstanceResolver() {
+            var c = new Container().Register<TestInterfaceA>(new TestClassA());
+            c.Register<DependantInterface>(new DependantClass(c.GetInstance<TestInterfaceA>()));
+            for (var i = 0; i < Iterations; i++)
+                c.GetInstance<DependantInterface>();
+        }
+
+        [Test]
+        public void MeasureFactoryInstanceResolver() {
+            var c = new Container();
+            c.Register<DependantInterface>(container => new DependantClass(container.GetInstance<TestInterfaceA>()));
+            c.Register<TestInterfaceA>(container => new TestClassA());
+            for (var i = 0; i < Iterations; i++)
+                c.GetInstance<DependantInterface>();
+        }
+
+    }
+}
---
+++ NContainerTests/NContainerTests/UnitTests.cs
@@ -0,0 +1,90 @@
+﻿using System;
+using System.Collections;
+using FluentAssertions;
+using NContainer;
+using NUnit.Framework;
+
+namespace NContainerTests {
+    [TestFixture]
+    public class UnitTests {
+        [Test]
+        public void HappyPathInterfaceAndClassPair() {
+            var c = new Container();
+            c.Register<TestInterfaceA, TestClassA>();
+            c.GetInstance<TestInterfaceA>().Should().BeOfType<TestClassA>();
+        }
+
+        [Test]
+        public void HappyPathClassRegistration() {
+            var c = new Container();
+            c.Register<TestClassA>();
+            c.GetInstance<TestInterfaceA>().Should().BeOfType<TestClassA>();
+        }
+
+        [Test]
+        public void HappyPathClassWithDependencies() {
+            var c = new Container();
+            c.Register<TestClassA>().Register<DependantClass>();
+            c.GetInstance<DependantInterface>().Should().BeOfType<DependantClass>();
+        }
+
+        [Test]
+        public void MissingDependencyThrowsException() {
+            var c = new Container();
+            c.Register<DependantClass>();
+            Action getInstance = () => c.GetInstance<DependantInterface>();
+            getInstance.ShouldThrow<UnresolvedInterfaceException>();
+
+        }
+
+        [Test]
+        public void HappyPathInterfaceWithFactoryMethod() {
+            var c = new Container();
+            c.Register<TestInterfaceA>(myContainer => new TestClassA());
+            c.GetInstance<TestInterfaceA>().Should().BeOfType<TestClassA>();
+        }
+
+        [Test]
+        public void HappyPathSingleInstance() {
+            var c = new Container();
+            var myInstance = new TestClassA();
+            c.Register<TestInterfaceA>(myInstance);
+            c.GetInstance<TestInterfaceA>().Should().BeSameAs(myInstance);
+        }
+
+        [Test]
+        public void MultipleRequests() {
+            var c = new Container();
+            var myInstance = new TestClassA();
+            c.Register<TestInterfaceA>(myInstance);
+            c.Register<TestInterfaceA>(myInstance);
+            var myVariable = c.GetInstance<TestInterfaceA>();
+            var myVariableb = c.GetInstance<TestInterfaceA>();
+            Assert.AreSame(myVariable, myVariableb);
+        }
+
+        [Test]
+        public void IsRegisteredReturnsFalseForUnregisteredInterfaces() => 
+            new Container().IsRegistered<TestInterfaceA>().Should().BeFalse();
+
+        
+
+        [Test]
+        public void IsRegisteredReturnsTrueForRegisteredInterfaces() =>
+            new Container().Register<TestInterfaceA>(new TestClassA()).IsRegistered<TestInterfaceA>().Should().BeTrue();
+       
+        [Test]
+        public void MissingPublicConstructorThrowsException() {
+            var c = new Container();
+            c.Register<NonPublicConstructorClass>();
+            Assert.Throws<MissingPublicConstructorException>(() => c.GetInstance<TestInterfaceA>());
+        }
+
+        [Test]
+        public void UnregisteredContractThrowsException() {
+            var c = new Container();
+            Assert.Throws<UnresolvedInterfaceException>(() => c.GetInstance<IEnumerable>());
+        }
+    }
+}
+
